e18500788323b62017de447b03991f4d5eda6a9d
Muhammad-Bilal2000 <82469386+Muhammad-Bilal2000@users.noreply.github.com>
2022-04-18 05:30:53 +0500

    Add files via upload

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,68 @@
+<template>
+<div></div>
+  <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img class="logo" alt="Vue logo" src="../assets/logo.png" />
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <li class="nav-item" v-if="!isAuthenticated">
+          <router-link class="nav-link" to="/login">Login</router-link>
+        </li>
+        <li class="nav-item" v-if="!isAuthenticated">
+          <router-link class="nav-link" to="/signup">Signup</router-link>
+        </li>
+        <li class="nav-item" v-if="isAuthenticated">
+          <router-link class="nav-link" to="/posts">Posts</router-link>
+        </li>
+        <li class="nav-item" v-if="isAuthenticated">
+          <a href="#" class="nav-link" @click.prevent="onLogout()">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </nav> -->
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import {
+  IS_USER_AUTHENTICATE_GETTER,
+  LOGOUT_ACTION,
+} from "../store/storeconstants";
+export default {
+  computed: {
+    ...mapGetters("auth", {
+      isAuthenticated: IS_USER_AUTHENTICATE_GETTER,
+    }),
+  },
+  methods: {
+    ...mapActions("auth", {
+      logout: LOGOUT_ACTION,
+    }),
+    onLogout() {
+      this.logout();
+      this.$router.replace("/login");
+    },
+  },
+};
+</script>
+<style>
+.logo {
+  height: 50%;
+  width: 100px;
+  padding: 0% !important;
+  margin: 0%;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- <div>Welcome Page</div> -->
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Welcome to Vue.js Todo App</h1>
+    <button type="submit" class="btn btn-primary">
+      <router-link class="nav-link" to="/login">Go to login page</router-link>
+    </button>
+    <!-- <div>
+            <router-link to="/posts">Go to posts page</router-link>
+    </div> -->
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+img {
+  padding-top: 10%;
+}
+.nav-link{
+    color: white;
+}
+.nav-link :hover{
+    color: rgb(231, 210, 210);
+}
+</style>
